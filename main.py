719d04c16a289ef394ea5fd4115578348be93e71
CHY <824268898@qq.com>
2022-09-21 00:48:06 +0800

    第一次提交

---
+++ main.py
@@ -0,0 +1,46 @@
+import jieba
+import re
+import gensim
+
+
+
+def sentenceclean(path):
+    str = ''
+    f = open(path, 'r', encoding='UTF-8')
+    line = f.readline()
+    while line:
+        str = str + line
+        line = f.readline()
+    '''jieba中文分词'''
+    sentence1 = jieba.lcut(str)
+    '''由于是中文文本的查重，把除中文外的符号全部清除'''
+    d = "[\u4e00-\u9fa5]+"
+    sentence2 = []
+    for i in sentence1:
+        a = re.findall(d, i)
+        sentence2 += a
+    f.close()
+    return sentence2
+
+
+def calc_similarity(str1, str2):
+    texts = [str1, str2]
+    '''建立字典'''
+    dictionary = gensim.corpora.Dictionary(texts)
+    '''使用doc2bow制作语料库.
+    语料库是一组向量，向量中的元素是一个二元组（编号、频次数），对应分词后的文档中的每一个词。'''
+    corpus = [dictionary.doc2bow(text) for text in texts]
+    '''similarities.MatrixSimilarity类仅仅适合能将所有的向量都在内存中的情况。
+       例如，如果一个百万文档级的语料库使用该类，可能需要2G内存与256维LSI空间。 
+       如果没有足够的内存，你可以使用similarities.Similarity类。
+       该类的操作只需要固定大小的内存，因为他将索引切分为多个文件（称为碎片）存储到硬盘上了。
+       它实际上使用了similarities.MatrixSimilarity和similarities.SparseMatrixSimilarity两个类。
+       因此它也是比较快的，虽然看起来更加复杂了。
+       similarity用于计算相似度，返回的是一组列表'''
+    similarity = gensim.similarities.Similarity('-Similarity-index', corpus, num_features=len(dictionary))
+    '''计算text1的向量'''
+    test_corpus_1 = dictionary.doc2bow(str1)
+    '''计算测试文档的相似度'''
+    cosine_sim = similarity[test_corpus_1][1]
+    return cosine_sim
+
